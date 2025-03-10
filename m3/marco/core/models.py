430cbea9800b0852178f5ca3cadad1c68f5459c9
Marco Monteiro <marcomont225@gmail.com>
2025-03-10 17:52:03 -0300

    10/03/25

--- m3/marco/core/models.py
+++ m3/marco/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+class Base(models.Model):
+    criado = models.DateField("Criação", auto_now_add=True)
+    modificado = models.DateField("Modificado", auto_now=True)
+    ativo = models.BooleanField("Ativo", default=True)
+
+    class Meta:
+        abstract = True
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog','Engrenagem'),
+        ('lni-stats-up','Grafico'),
+        ('lni-users','Usuarios'),
+        ('lni-layers','Design'),
+        ('lni-mobile','Mobile'),
+        ('lni-rocket','Foguete'),
+    )
+    servico = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField("descrição", max_length=200)
+    icone = models.CharField("Icone", max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.servico
