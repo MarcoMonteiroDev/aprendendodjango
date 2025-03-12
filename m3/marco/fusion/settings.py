3d80324cc5a2236b756a1d30398bb47a836705b0
Marco Monteiro <marcomont225@gmail.com>
2025-03-12 10:38:57 -0300

    email

--- m3/marco/fusion/settings.py
+++ m3/marco/fusion/settings.py
@@ -130,3 +130,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#email teste console
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # aqui pra teste se for publicar descomentar as proximas
+
+"""
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no-reply@fusion.com.br'
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+EMAIL_HOST_PASSWORD = 'fusion'
+DEFAULT_FROM_EMAIL = 'contato@fusion.com.br
+"""
