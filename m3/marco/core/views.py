8b8c1b8bed80266b944c3eedc77fb2841a1292bf
Marco Monteiro <marcomont225@gmail.com>
2025-03-12 08:48:19 -0300

    dados apresentados no template

--- m3/marco/core/views.py
+++ m3/marco/core/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from .models import Servico, Funcionario
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
