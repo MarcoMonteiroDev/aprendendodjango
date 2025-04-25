08e743a73c3352b0902415bb0416982fafc5aabd
Marco Monteiro <marcomont225@gmail.com>
2025-04-25 19:25:21 -0300

    25/04/25

--- mc/venv/usuarios/forms.py
+++ mc/venv/usuarios/forms.py
@@ -7,14 +7,14 @@ class CustomUsuarioCreateForm(UserCreationForm):
         model = CustomUsuario
         fields = ('first_name', 'last_name', 'fone')
         labels= {'username': 'Username/E-mail'}
-""" 
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
         user.email = self.cleaned_data['username']
         if commit:
             user.save()
-        return user """
+        return user
     
 class CustomUsuarioChangeForm(UserChangeForm):
 
